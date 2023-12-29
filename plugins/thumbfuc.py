1313f14e01177abdf98a7d78751d127778e06d60
VIIZET <129474307+viizet@users.noreply.github.com>
2023-12-29 18:53:09 +0300

    Update thumbfuc.py

--- plugins/thumbfuc.py
+++ plugins/thumbfuc.py
@@ -1,100 +1,27 @@
 from pyrogram import Client, filters
 from helper.database import db
 
-import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
-import random
-import numpy
-import os
-from PIL import Image
-import time
-
-from config import Config
-# the Strings used for this "thing"
-from plugins.startmsg import Translation
-from pyrogram import Client as Clinton
-from database.access import clinton
-from hachoir.metadata import extractMetadata
-from hachoir.parser import createParser
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-from pyrogram import filters
-from database.adduser import AddUser
-from helper_funcs.help_Nekmo_ffmpeg import take_screen_shot
-    
-    
-    
-    
-    
-    
-    @Client.on_message(filters.private & filters.photo)
-async def save_photo(bot, update):
-    await AddUser(bot, update)
-    await clinton.set_thumbnail(update.from_user.id, thumbnail=update.photo.file_id)
-    await bot.send_message(chat_id=update.chat.id, text=Translation.SAVED_CUSTOM_THUMB_NAIL, reply_to_message_id=update.message_id)
-
-@Client.on_message(filters.private & filters.command("delthumb"))
-async def removethumb(bot, update):
-    await AddUser(bot, update)
-    await clinton.set_thumbnail(update.from_user.id, thumbnail=None)
-    await bot.send_message(chat_id=update.chat.id, text=Translation.DEL_ETED_CUSTOM_THUMB_NAIL, reply_to_message_id=update.message_id)
-
-@Client.on_message(filters.private & filters.command("viewthumb") )
-async def viewthumb(bot, update):
-    await AddUser(bot, update)
-    thumbnail = await clinton.get_thumbnail(update.from_user.id)
-    if thumbnail is not None:
-        await bot.send_photo(
-        chat_id=update.chat.id,
-        photo=thumbnail,
-        caption=f"Saved your thumbnail 😊",
-        reply_to_message_id=update.message_id)
+@Client.on_message(filters.private & filters.command(['viewthumb']))
+async def viewthumb(client, message):    
+    thumb = await db.get_thumbnail(message.from_user.id)
+    if thumb:
+       await client.send_photo(
+	   chat_id=message.chat.id, 
+	   photo=thumb)
     else:
-        await update.reply_text(text=f"No thumbnail found 🥴")
-
-async def Gthumb01(bot, update):
-    thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
-    db_thumbnail = await clinton.get_thumbnail(update.from_user.id)
-    if db_thumbnail is not None:
-        thumbnail = await bot.download_media(message=db_thumbnail, file_name=thumb_image_path)
-        Image.open(thumbnail).convert("RGB").save(thumbnail)
-        img = Image.open(thumbnail)
-        img.resize((100, 100))
-        img.save(thumbnail, "JPEG")
-    else:
-        thumbnail = None
-
-    return thumbnail
-
-async def Gthumb02(bot, update, duration, download_directory):
-    thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
-    db_thumbnail = await clinton.get_thumbnail(update.from_user.id)
-    if db_thumbnail is not None:
-        thumbnail = await bot.download_media(message=db_thumbnail, file_name=thumb_image_path)
-    else:
-        thumbnail = await take_screen_shot(download_directory, os.path.dirname(download_directory), random.randint(0, duration - 1))
-
-    return thumbnail
-
-async def Mdata01(download_directory):
-
-          width = 0
-          height = 0
-          duration = 0
-          metadata = extractMetadata(createParser(download_directory))
-          if metadata is not None:
-              if metadata.has("duration"):
-                  duration = metadata.get('duration').seconds
-              if metadata.has("width"):
-                  width = metadata.get("width")
-              if metadata.has("height"):
-                  height = metadata.get("height")
-
-          return width, height, duration
-
-async def Mdata02(download_directory):
+        await message.reply_text("😔**Sorry ! No thumbnail found...**😔") 
+		
+@Client.on_message(filters.private & filters.command(['delthumb']))
+async def removethumb(client, message):
+    await db.set_thumbnail(message.from_user.id, file_id=None)
+    await message.reply_text("**Thumbnail deleted successfully**✅️")
+	
+@Client.on_message(filters.private & filters.photo)
+async def addthumbs(client, message):
+    LazyDev = await message.reply_text("Please Wait ...")
+    await db.set_thumbnail(message.from_user.id, file_id=message.photo.file_id)                
+    await LazyDev.edit("**Thumbnail saved successfully**✅️")
+	
 
           width = 0
           duration = 0
